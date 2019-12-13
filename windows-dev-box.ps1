a4bbf3c9f50f9f060466aa7946a69819aec44d1b
James Collins <james.x.collins@gmail.com>
2019-12-13 14:25:41 +1100

    Create windows-dev-box.ps1

---
+++ windows-dev-box.ps1
@@ -0,0 +1,31 @@
+# Disable UAC for unattended install
+Disable-UAC
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+##### Install Software #####
+
+# Install browsers
+choco install -y brave
+choco install -y firefox
+choco install -y googlechrome
+
+#--- reenabling critial items ---
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
