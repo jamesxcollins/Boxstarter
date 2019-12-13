6a547b7e55a8e617c3828641ac4e5d57ad8fd44f
James Collins <james.x.collins@gmail.com>
2019-12-13 16:00:31 +1100

    Create CommonDevTools.ps1

---
+++ scripts/CommonDevTools.ps1
@@ -0,0 +1,6 @@
+# Tools common across dev workloads
+choco install -y vscode
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y python
+choco install -y 7zip.install
+choco install -y sysinternals
