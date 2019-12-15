8f3136baa555ec4ea42e708baa52497a10718c81
James Collins <james.x.collins@gmail.com>
2019-12-16 10:01:33 +1100

    Create VisualStudio.ps1

---
+++ scripts/VisualStudio.ps1
@@ -0,0 +1,9 @@
+# Install Visual Studio and workloads
+choco install -y visualstudio2019professional
+choco install -y visualstudio2019-workload-azure
+choco install -y visualstudio2019-workload-data
+choco install -y visualstudio2019-workload-manageddesktop
+choco install -y visualstudio2019-workload-netcoretools
+choco install -y visualstudio2019-workload-netweb
+choco install -y visualstudio2019-workload-python
+choco install -y ssis-vs2019
