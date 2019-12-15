7a0bf086dd7de607f049c275ba5d074af0c71d3f
James Collins <james.x.collins@gmail.com>
2019-12-16 09:21:12 +1100

    Create WindowsExtras.ps1

---
+++ scripts/WindowsExtras.ps1
@@ -0,0 +1,5 @@
+# Some extra tools for development or mangement of windows devices
+choco install -y microsoft-windows-terminal
+choco install -y powershell-core
+choco install -y windows-admin-center
+choco install -y wsl-ubuntu-1804
