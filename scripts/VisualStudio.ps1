9d73948e9c56a169e75272dccc9ac5289339c490
James Collins <james.x.collins@gmail.com>
2019-12-16 10:06:12 +1100

    Update VisualStudio.ps1

--- scripts/VisualStudio.ps1
+++ scripts/VisualStudio.ps1
@@ -7,3 +7,6 @@ choco install -y visualstudio2019-workload-netcoretools
 choco install -y visualstudio2019-workload-netweb
 choco install -y visualstudio2019-workload-python
 choco install -y ssis-vs2019
+
+# Add Barker nuget feed
+nuget sources Add -Name "BarkerFeed" -Source https://pkgs.dev.azure.com/BarkerCollegeDev/_packaging/BarkerFeed/nuget/v3/index.json
