1245197fb18fbe1f195092f352a328c5f3efcb1f
James Collins <james.x.collins@gmail.com>
2019-12-16 09:50:28 +1100

    Update WindowsExtras.ps1

--- scripts/WindowsExtras.ps1
+++ scripts/WindowsExtras.ps1
@@ -1,6 +1,5 @@
 # Some extra tools for development or mangement of windows devices
 choco install -y microsoft-windows-terminal
-choco install -y powershell-core
-# Windows Admin Center package is not installing correctly. Comment out for now.
-#choco install -y windows-admin-center
+Windows Admin Center package is not installing correctly. Comment out for now.
+choco install -y windows-admin-center
 choco install -y wsl-ubuntu-1804
