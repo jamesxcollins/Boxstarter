2c0d8219908cc7c3fdfea3f0f30cb5c9f3ee7466
James Collins <james.x.collins@gmail.com>
2019-12-16 10:55:54 +1100

    Update SystemConfig.ps1

--- scripts/SystemConfig.ps1
+++ scripts/SystemConfig.ps1
@@ -1,2 +1,5 @@
 #--- Enable developer mode on the system ---
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+
+#--- Add Run As Different User option
+Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\Explorer -Name ShowRunasDifferentuserinStart -Value 1
