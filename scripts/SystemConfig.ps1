f26b5bf3fe6d64e7fd53d269062486e94f5b6e06
James Collins <james.x.collins@gmail.com>
2020-01-14 16:11:36 +1100

    Update SystemConfig.ps1

--- scripts/SystemConfig.ps1
+++ scripts/SystemConfig.ps1
@@ -2,4 +2,4 @@
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
 
 #--- Add Run As Different User option
-Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\Explorer -Name ShowRunasDifferentuserinStart -Value 1
+Set-ItemProperty -Path HKCU:\Software\Policies\Microsoft\Windows\Explorer -Name ShowRunasDifferentuserinStart -Value 1
